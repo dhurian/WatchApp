68f3662a170b4931425fd3a9e2fc06beeca8f532
dhurian <dhurian@gmail.com>
2026-03-01 08:00:57 +0100

    Initial Commit

---
+++ WatchApp/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp
+//
+//  Created by Dhurian Vitoldas on 01/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct WatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
